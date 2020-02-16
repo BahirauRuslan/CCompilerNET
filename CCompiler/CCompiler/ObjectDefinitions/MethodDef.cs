420bb61873211a8b19b6fb65c88f212a75c5059e
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-02-16 19:49:32 +0300

    Create MethodDef.cs

---
+++ CCompiler/CCompiler/ObjectDefinitions/MethodDef.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection.Emit;
+
+namespace CCompiler.ObjectDefinitions
+{
+    public class MethodDef
+    {
+        public MethodDef(
+            string name,
+            Dictionary<string, MethodArgDef> args,
+            MethodBuilder methodBuilder)
+        {
+            Name = name;
+            Args = args;
+            MethodBuilder = methodBuilder;
+        }
+
+        public string Name { get; set; }
+
+        public MethodBuilder MethodBuilder { get; set; }
+
+        public Dictionary<string, MethodArgDef> Args { get; set; }
+
+    }
+}
