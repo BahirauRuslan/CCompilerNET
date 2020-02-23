bd89cec5b3cec0ee015a1a125b72b3edd6836a50
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-02-23 17:08:05 +0300

    Create BlockItemListExtension.cs

---
+++ CCompiler/CCompiler/Extensions/BlockItemListExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using CCompiler.ObjectDefinitions;
+
+namespace CCompiler.Extensions
+{
+    public static class BlockItemListExtension
+    {
+        public static Stack<CParser.BlockItemContext> RBABlockItemStack(
+            this CParser.BlockItemListContext blockItemList)
+        {
+            var localBlockItemList = blockItemList;
+            var blockItemStack
+                = new Stack<CParser.BlockItemContext>();
+
+            while (localBlockItemList.blockItemList() != null)
+            {
+                blockItemStack
+                    .Push(localBlockItemList.blockItem());
+
+                localBlockItemList = localBlockItemList.blockItemList();
+            }
+
+            blockItemStack
+                .Push(localBlockItemList.blockItem());
+
+            return blockItemStack;
+        }
+    }
+}
