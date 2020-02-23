85309660b0bd693f3ca699ca504d6a3c2b431f9c
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-02-23 15:04:14 +0300

    Create TranslationUnitExtension.cs

---
+++ CCompiler/CCompiler/Extensions/TranslationUnitExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using CCompiler.ObjectDefinitions;
+
+namespace CCompiler.Extensions
+{
+    public static class TranslationUnitExtension
+    {
+        public static Stack<CParser.ExternalDeclarationContext>
+            RBAExternalDeclarationStack(
+            this CParser.TranslationUnitContext translationUnit)
+        {
+            var localTranslationUnit = translationUnit;
+            var externalDeclarationStack
+                = new Stack<CParser.ExternalDeclarationContext>();
+
+            while (localTranslationUnit.translationUnit() != null)
+            {
+                externalDeclarationStack
+                    .Push(localTranslationUnit.externalDeclaration());
+
+                localTranslationUnit = localTranslationUnit.translationUnit();
+            }
+
+            externalDeclarationStack
+                .Push(localTranslationUnit.externalDeclaration());
+
+            return externalDeclarationStack;
+        }
+    }
+}
