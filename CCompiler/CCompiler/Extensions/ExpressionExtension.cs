9aced73896747c17e8639edda1d823bfabe3c25a
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-03-24 20:27:39 +0300

    Create ExpressionExtension.cs

---
+++ CCompiler/CCompiler/Extensions/ExpressionExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using CCompiler.ObjectDefinitions;
+
+namespace CCompiler.Extensions
+{
+    public static class ExpressionExtension
+    {
+        public static Stack<CParser.AssignmentExpressionContext>
+            RBAAssignmentExpressionStack(
+            this CParser.ExpressionContext expressionContext)
+        {
+            var localExpressionContext = expressionContext;
+            var assignmentExpressionStack
+                = new Stack<CParser.AssignmentExpressionContext>();
+
+            while (localExpressionContext.expression() != null)
+            {
+                assignmentExpressionStack
+                    .Push(localExpressionContext.assignmentExpression());
+
+                localExpressionContext = localExpressionContext.expression();
+            }
+
+            assignmentExpressionStack
+                .Push(localExpressionContext.assignmentExpression());
+
+            return assignmentExpressionStack;
+        }
+    }
+}
