1c139f22daf0567a6611f54a36175d1979213378
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-04-10 11:37:28 +0300

    Create ConsoleApp.cs

---
+++ CCompiler/CCompiler/ConsoleApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace CCompiler
+{
+    public class ConsoleApp : BaseApp
+    {
+        public ConsoleApp(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        public override void Run()
+        {
+            using (var stream = new FileStream(FileName, FileMode.Open))
+            {
+                this.RunCompile(stream);
+            }
+        }
+    }
+}
