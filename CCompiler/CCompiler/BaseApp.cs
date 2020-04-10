5a6531b117b60127dd88521da2eff68af982649e
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-04-10 11:37:23 +0300

    Create BaseApp.cs

---
+++ CCompiler/CCompiler/BaseApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace CCompiler
+{
+    public abstract class BaseApp
+    {
+        protected string FileName { get; set; }
+
+        protected string RunCompile(Stream stream)
+        {
+            string result = null;
+
+            try
+            {
+                var compiler = new CCompiler(stream, FileName);
+
+                compiler.Compile();
+
+                result = compiler.OutputFileName;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Uncatched error:\n { e.Message }");
+            }
+
+            return result;
+        }
+
+        public abstract void Run();
+    }
+}
