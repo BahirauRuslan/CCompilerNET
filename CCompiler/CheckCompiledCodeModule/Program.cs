4a807528bf97513df9d8c77d17a00bc7f407298f
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-05-13 18:03:25 +0300

    Create Program.cs

---
+++ CCompiler/CheckCompiledCodeModule/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CheckCompiledCodeModule
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Code.Code code = new Code.Code();
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("{0}", code.func54());
+            Console.WriteLine("{0}", code.funcParen());
+            Console.WriteLine("{0}", code.funcFunc());
+            
+            
+            Console.WriteLine("{0}", code.funcFunc4());
+            
+            Console.WriteLine("{0}", code.funcFunc3());
+            Console.WriteLine("{0}", code.funcFunc2());
+            Console.WriteLine("{0}", code.lalala());
+        }
+    }
+}
