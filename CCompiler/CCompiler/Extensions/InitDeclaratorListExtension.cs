4bb77d8182ee04cb74113c9e777e75a9743d8b18
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-05-13 18:03:20 +0300

    Create InitDeclaratorListExtension.cs

---
+++ CCompiler/CCompiler/Extensions/InitDeclaratorListExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using CCompiler.ObjectDefinitions;
+
+namespace CCompiler.Extensions
+{
+    public static class InitDeclaratorListExtension
+    {
+        public static Stack<CParser.InitDeclaratorContext> RBAInitDeclaratorStack(
+            this CParser.InitDeclaratorListContext initDeclaratorList)
+        {
+            var localInitDeclaratorList = initDeclaratorList;
+            var initDeclaratorStack
+                = new Stack<CParser.InitDeclaratorContext>();
+
+            while (localInitDeclaratorList.initDeclaratorList() != null)
+            {
+                initDeclaratorStack
+                    .Push(localInitDeclaratorList.initDeclarator());
+
+                localInitDeclaratorList = localInitDeclaratorList.initDeclaratorList();
+            }
+
+            initDeclaratorStack
+                .Push(localInitDeclaratorList.initDeclarator());
+
+            return initDeclaratorStack;
+        }
+    }
+}
