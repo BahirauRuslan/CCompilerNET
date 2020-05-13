9782b883a9308e5415e976028dc79f86238fa3ab
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-05-13 18:03:15 +0300

    Create DeclarationExtension.cs

---
+++ CCompiler/CCompiler/Extensions/DeclarationExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using CCompiler.ObjectDefinitions;
+
+namespace CCompiler.Extensions
+{
+    public static class DeclarationExtension
+    {
+        public static CParser.TypeSpecifierContext RBATypeSpecifier(
+            this CParser.DeclarationContext declaration)
+        {
+            return declaration
+                ?.declarationSpecifiers()
+                ?.declarationSpecifier()?[0]
+                ?.typeSpecifier();
+        }
+    }
+}
