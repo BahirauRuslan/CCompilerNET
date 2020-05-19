bedca8d28ca08523ca1a90c263d0b48037a15899
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-05-19 08:58:26 +0300

    Create ParameterListExtension.cs

---
+++ CCompiler/CCompiler/Extensions/ParameterListExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace CCompiler.Extensions
+{
+    public static class ParameterListExtension
+    {
+        public static Stack<CParser.ParameterDeclarationContext> RBAParameterDeclarationStack(
+            this CParser.ParameterListContext parameterList)
+        {
+            var localParameterList = parameterList;
+            var parameterDeclarationStack
+                = new Stack<CParser.ParameterDeclarationContext>();
+
+            while (localParameterList.parameterList() != null)
+            {
+                parameterDeclarationStack
+                    .Push(localParameterList.parameterDeclaration());
+
+                localParameterList = localParameterList.parameterList();
+            }
+
+            parameterDeclarationStack
+                .Push(localParameterList.parameterDeclaration());
+
+            return parameterDeclarationStack;
+        }
+    }
+}
