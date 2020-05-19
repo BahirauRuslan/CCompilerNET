13fa08788253fa8b45044c7d6bd18d0d3bdcb825
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-05-19 08:59:40 +0300

    Update DeclarationExtension.cs

--- CCompiler/CCompiler/Extensions/DeclarationExtension.cs
+++ CCompiler/CCompiler/Extensions/DeclarationExtension.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
-using Antlr4.Runtime;
-using Antlr4.Runtime.Tree;
-using CCompiler.ObjectDefinitions;
-
-namespace CCompiler.Extensions
+﻿namespace CCompiler.Extensions
 {
     public static class DeclarationExtension
     {
