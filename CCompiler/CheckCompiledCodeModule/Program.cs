1e4f3d41622cd8f5240999da7107cff2e6dcc46e
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-05-19 08:59:10 +0300

    Update Program.cs

--- CCompiler/CheckCompiledCodeModule/Program.cs
+++ CCompiler/CheckCompiledCodeModule/Program.cs
@@ -19,6 +19,15 @@ namespace CheckCompiledCodeModule
             Console.WriteLine("{0}", code.funcFunc3());
             Console.WriteLine("{0}", code.funcFunc2());
             Console.WriteLine("{0}", code.lalala());
+            Console.WriteLine("{0}", code.lalala1());
+
+            Console.WriteLine("Fibonazzi numbers");
+            Console.WriteLine("{0}", code.fibonazzi(1));
+            Console.WriteLine("{0}", code.fibonazzi(2));
+            Console.WriteLine("{0}", code.fibonazzi(3));
+            Console.WriteLine("{0}", code.fibonazzi(4));
+            Console.WriteLine("{0}", code.fibonazzi(5));
+            Console.WriteLine("54th number: {0}", code.fibonazzi(8));
         }
     }
 }
