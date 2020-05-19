f3d89e31470d40a6d35bd68d5957c7d764291720
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2020-05-19 08:58:30 +0300

    Create ParameterDeclarationExtension.cs

---
+++ CCompiler/CCompiler/Extensions/ParameterDeclarationExtension.cs
@@ -0,0 +1,25 @@
+﻿using Antlr4.Runtime.Tree;
+
+namespace CCompiler.Extensions
+{
+    public static class ParameterDeclarationExtension
+    {
+        public static CParser.TypeSpecifierContext RBATypeSpecifier(
+            this CParser.ParameterDeclarationContext parameterDeclaration)
+        {
+            return parameterDeclaration
+                ?.declarationSpecifiers()
+                ?.declarationSpecifier()?[0]
+                ?.typeSpecifier();
+        }
+
+        public static ITerminalNode RBAIdentifier(
+            this CParser.ParameterDeclarationContext parameterDeclaration)
+        {
+            return parameterDeclaration
+                ?.declarator()
+                ?.directDeclarator()
+                ?.Identifier();
+        }
+    }
+}
